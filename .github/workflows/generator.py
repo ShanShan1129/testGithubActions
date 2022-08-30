23e3bba6079c4837d9fdae356a327d82e820b793
ShanShan1129 <82037936+ShanShan1129@users.noreply.github.com>
2022-08-30 22:45:43 +0900

    Update generator.py

--- .github/workflows/generator.py
+++ .github/workflows/generator.py
@@ -111,11 +111,11 @@ def generate_li(path,classID,addLI=False):
     #html生成
     for i,dir in enumerate(files_dir):
         localClassID=classID+"-"+str(i)
-        #print((addLI if '<li>' else '')+'<label for="'+localClassID+'">'+dir+"</label>")#      <label for="item1">項目１</label>
-        #print('<input type="checkbox" id="'+localClassID+'">')#        <input type="checkbox" id="item3">
-        #print('<ul>')
+        print((addLI if '<li>' else '')+'<label for="'+localClassID+'">'+dir+"</label>")#      <label for="item1">項目１</label>
+        print('<input type="checkbox" id="'+localClassID+'">')#        <input type="checkbox" id="item3">
+        print('<ul>')
         generate_li(path+"/"+dir,'logItem',True)
-        #print('/ul')
+        print('/ul')
 
     return
 
