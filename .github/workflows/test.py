a9077bb1251f5be1d72b3b3ba548818f73915765
ShanShan1129 <82037936+ShanShan1129@users.noreply.github.com>
2022-08-30 18:49:46 +0900

    Update test.py

--- .github/workflows/test.py
+++ .github/workflows/test.py
@@ -4,3 +4,9 @@ files = os.listdir(path)
 files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
 print(files_dir)
 print('ログ' not in files_dir)
+
+path='./ログ'
+files = os.listdir(path)
+files_dir = [f for f in files if os.path.isdir(os.path.join(path, f))]
+print(files_dir)
+print('ログ' not in files_dir)
